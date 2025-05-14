fed29512c4f95b0bc22fd8f41f8aa28baa6a49fc
Luciano Girotti <onaicul2008@gmail.com>
2025-05-14 15:33:08 -0300

    Calculadora Par o Impar

---
+++ Assets/Scripts/CalculadoraPoI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class CalculadoraPoI : MonoBehaviour
+{
+    public int num1;
+    public TMP_InputField inputNum1;
+    public TextMeshProUGUI textResultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textResultado.text = "";
+        Debug.Log("Loaded!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CalcularPoI()
+    {
+        num1 = int.Parse(inputNum1.text);
+        Debug.Log(num1);
+
+        if (num1 % 2 == 0)
+        {
+            textResultado.text = "Par";
+            Debug.Log("par");
+
+        }
+        else
+        {
+            textResultado.text = "Impar";
+            Debug.Log("imp");
+
+        }
+    }
+
+}
